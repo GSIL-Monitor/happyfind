3324cd64be49da417ac167405168eb67c234d6d5
liyang417800 <317616515@qq.com>
2015-08-17 00:07:10 +0800

    举报

--- src/happyfind/utils/CommonData.java
+++ src/happyfind/utils/CommonData.java
@@ -5,6 +5,8 @@ public class CommonData {
 	public static final String UPDATEBORROW="updateBorrow" ;
 	public static final String ADDREWARD="addReward" ;
 	public static final String UPDATEREWARD="updateReward" ;
+	public static final String ADDRELEASE="addRelease" ;
+	public static final String UPDATERELEASE="updateRelease" ;
 	
 	public static String getLogKey(String key){
 		String keyValue=null;
@@ -16,6 +18,10 @@ public class CommonData {
 			keyValue="添加悬赏信息";
 		}else if(key.equals("updateReward")){
 			keyValue="修改悬赏信息";
+		}else if(key.equals("addRelease")){
+			keyValue="增加举报信息";
+		}else if(key.equals("updateRelease")){
+			keyValue="修改举报信息";
 		}
 		return keyValue;
 	}
